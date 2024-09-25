011140f2c403f01867ba82483c666be0bce15933
chinara mammadova <cmmmdova739@gmail.com>
2024-09-25 18:10:57 +0000

    api

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ $(document).ready(function () {
 
     $('#btn').click(function (e) {
         e.preventDefault();
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("myWeather").val()}&appid=fe57130549e94791f490784ddc5b1ff8&units-metric`
-        async function myweather() {
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=c26930c831e40ee5f58acf9b71786623&units=metric`
+        async function myWeather() {
             let responce = await fetch(url)
             let data = await responce.json()
             $('#city').html(data.name)
             $("#temp").html(data.main.temp)
+            $("#feels").html(data.sys.id)
             console.log(data)
         }
-        myweather()
+        myWeather()
 
     });
 
