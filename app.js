ffec95f49f59d85242b3c54dff8a095ef90203cf
chinara mammadova <cmmmdova739@gmail.com>
2024-09-26 13:40:56 +0000

    weather

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 "use strict";
 $(document).ready(function () {
 
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=c26930c831e40ee5f58acf9b71786623&units=metric`
+            async function myWeather() {
+                let responce = await fetch(url)
+                let data = await responce.json()
+                $('#city').html(data.name)
+                $("#temp").html(data.main.temp)
+                $("#feels").html(data.main.feels_like)
+                $("#myWeather").val("")
+                console.log(data)
+            }
+            myWeather()
+        }
+    });
+
+
     $('#btn').click(function (e) {
         e.preventDefault();
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=c26930c831e40ee5f58acf9b71786623&units=metric`
@@ -10,7 +27,8 @@ $(document).ready(function () {
             $('#city').html(data.name)
             $("#temp").html(data.main.temp)
             $("#feels").html(data.main.feels_like)
-            //console.log(data)
+            $("#myWeather").val("")
+            console.log(data)
         }
         myWeather()
 
