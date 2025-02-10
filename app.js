cdb1a761465dae309557907e4166f8341ea1dc1d
chinara mammadova <cmmmdova739@gmail.com>
2025-02-10 09:46:01 +0000

    response

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ $(document).ready(function () {
         e.preventDefault();
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=c26930c831e40ee5f58acf9b71786623&units=metric`
         async function myWeather() {
-            let responce = await fetch(url)
-            let data = await responce.json()
+            let response = await fetch(url)
+            let data = await response.json()
             $('#city').html(data.name)
             $("#temp").html(data.main.temp)
             $("#feels").html(data.main.feels_like)
